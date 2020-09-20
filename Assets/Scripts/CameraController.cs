3831ccf781f2e7ca5bdb44097724fd02bad9ec3b
Luiz Strobelt <lhsperez@gmail.com>
2020-09-19 23:33:11 -0300

    First version

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothSpeed = 10f;
+
+    private Vector3 offset; // Probably what I need to change using the mouse or right stick
+    private Vector3 velocity = Vector3.one;
+    private float heading = 0;
+
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        var desiredPosition = target.position + offset;
+        var smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity,
+            smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        heading += Input.GetAxis("Mouse X") * Time.deltaTime * 10;
+
+        Quaternion.Euler(0, heading, 0).ToAngleAxis(out float angle, out Vector3 axis);
+        transform.RotateAround(target.position, axis, angle);
+        transform.LookAt(target);
+
+        //var desiredPosition = target.position + offset;
+        //var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        //transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float PlayerSpeed = 5.0f;
+    public float JumpHeight = 5.0f;
+    public float GravityValue = -9.81f;
+
+    private CharacterController controller;
+    private Camera mainCamera;
+    private Vector3 playerVelocity;
+    private float movementThreshold = 1.0f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        var camForward = mainCamera.transform.forward;
+        var camRight = mainCamera.transform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+        var horizontalInput = camRight * hInput + camForward * vInput;
+        //new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        var horizontalVelocity = horizontalInput * PlayerSpeed;
+
+        horizontalVelocity = Vector3.MoveTowards(horizontalVelocity, mainCamera.transform.position,
+            PlayerSpeed * Time.deltaTime);
+
+        if (horizontalInput != Vector3.zero) transform.forward = horizontalInput;
+
+        playerVelocity = new Vector3(horizontalVelocity.x, playerVelocity.y, horizontalVelocity.z);
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+                playerVelocity.y += Mathf.Log(JumpHeight * -PlayerSpeed * GravityValue);
+
+            if (playerVelocity.y < 0) playerVelocity.y = 0;
+        }
+
+        playerVelocity.y += GravityValue * Time.deltaTime;
+
+        if (playerVelocity.magnitude > movementThreshold)
+            controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
