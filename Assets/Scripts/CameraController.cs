68dec7bb630aa963b2b0767a457b03cb36203214
Luiz Strobelt <lhsperez@gmail.com>
2020-09-22 00:04:18 -0300

    Clamp horizontal angle of vision

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,6 +6,11 @@ public class CameraController : MonoBehaviour
     public Transform Target;
     public float RotationSpeed = 1;
 
+    [Range(0, 360)]
+    public float MaxUpwardAngle = 65;
+    [Range(0, 360)]
+    public float MaxDownwardAngle = 320;
+
     private float _offsetDistance;
     private Vector2 _movementVector = Vector2.zero;
 
@@ -21,13 +26,45 @@ public class CameraController : MonoBehaviour
         var hInput = _movementVector.x;
         var vInput = _movementVector.y;
 
+        RotateAroundTarget(hInput, vInput);
+        LookAtTarget();
+
+        if (HorizontalAngleIsOutOfBounds())
+        {
+            RotateAroundTargetVertical(-vInput);
+            LookAtTarget();
+        }
+
+        DistanceFromTarget();
+    }
+
+    private bool HorizontalAngleIsOutOfBounds()
+    {
+        var x = transform.eulerAngles.x;
+        return MaxUpwardAngle < x && x < MaxDownwardAngle;
+    }
+
+    private void RotateAroundTarget(float hInput, float vInput)
+    {
         transform.RotateAround(Target.position, Vector3.up, hInput * RotationSpeed);
-        transform.RotateAround(Target.position, Vector3.left, vInput * -RotationSpeed);
+        RotateAroundTargetVertical(vInput);
+    }
+
+    private void RotateAroundTargetVertical(float vInput)
+    {
+        transform.RotateAround(Target.position, Vector3.left, vInput * RotationSpeed);
+    }
+
+    private void LookAtTarget()
+    {
         var angles = transform.localEulerAngles;
         angles.z = 0;
         transform.localEulerAngles = angles;
         transform.LookAt(Target);
+    }
 
+    private void DistanceFromTarget()
+    {
         var targetDirection = (transform.position - Target.position).normalized;
         var distance = targetDirection * _offsetDistance;
         transform.position = Target.position + distance;
