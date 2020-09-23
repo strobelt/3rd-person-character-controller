104fcf6355672a664421f1d23a91ed70a6c07be9
Luiz Strobelt <lhsperez@gmail.com>
2020-09-23 01:12:14 -0300

    Jumping and method extraction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,13 @@ public class PlayerMovement : MonoBehaviour
     public float JumpHeight = 5.0f;
     public float GravityValue = -9.81f;
 
+    private const float MovementThreshold = 1.0f;
+
     private CharacterController _controller;
     private Camera _mainCamera;
     private Vector3 _playerVelocity;
-    private readonly float _movementThreshold = 1.0f;
     private Vector2 _movementVector;
+    private bool _isJumping;
 
     private void Start()
     {
@@ -20,66 +22,60 @@ public class PlayerMovement : MonoBehaviour
         _mainCamera = Camera.main;
     }
 
-    void Update() => MovePlayer();
+    void Update() => HandlePlayerMovement();
 
     public void Move(InputAction.CallbackContext context)
         => _movementVector = context.ReadValue<Vector2>();
 
-    void MovePlayer()
+    public void Jump(InputAction.CallbackContext context) => _isJumping = context.started || context.performed;
+
+    void HandlePlayerMovement()
     {
-        var hInput = _movementVector.x;
-        var vInput = _movementVector.y;
-
-        var camForward = _mainCamera.transform.forward;
-        var camRight = _mainCamera.transform.right;
-        camForward.y = 0;
-        camRight.y = 0;
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
-
-        var horizontalInput = camRight * hInput + camForward * vInput;
-        var horizontalVelocity = horizontalInput * PlayerSpeed;
-
-        if (horizontalInput != Vector3.zero) transform.forward = horizontalInput;
-
-        _playerVelocity = new Vector3(horizontalVelocity.x, _playerVelocity.y, horizontalVelocity.z);
-
-        if (_playerVelocity.magnitude > _movementThreshold)
-            _controller.Move(_playerVelocity * Time.deltaTime);
-    }
-
-    void OldUpdate()
-    {
-        var camForward = _mainCamera.transform.forward;
-        var camRight = _mainCamera.transform.right;
-        camForward.y = 0;
-        camRight.y = 0;
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
-
-        var hInput = 0;
-        var vInput = 0;
-        var horizontalInput = camRight * hInput + camForward * vInput;
-        var horizontalVelocity = horizontalInput * PlayerSpeed;
-
-        horizontalVelocity = Vector3.MoveTowards(horizontalVelocity, _mainCamera.transform.position,
-            PlayerSpeed * Time.deltaTime);
-
-        if (horizontalInput != Vector3.zero) transform.forward = horizontalInput;
-
-        _playerVelocity = new Vector3(horizontalVelocity.x, _playerVelocity.y, horizontalVelocity.z);
+        CalculateHMovementRelativeToCam(_movementVector.x, _movementVector.y);
 
         if (_controller.isGrounded)
         {
-            if (Input.GetButtonDown("Jump"))
-                _playerVelocity.y += Mathf.Log(JumpHeight * -PlayerSpeed * GravityValue);
-
-            if (_playerVelocity.y < 0) _playerVelocity.y = 0;
+            Jump();
+            StopFalling();
         }
 
-        _playerVelocity.y += GravityValue * Time.deltaTime;
+        Fall();
 
-        if (_playerVelocity.magnitude > _movementThreshold)
-            _controller.Move(_playerVelocity * Time.deltaTime);
+        if (ShouldMove())
+            MovePlayer();
     }
+
+    private void CalculateHMovementRelativeToCam(float hInput, float vInput)
+    {
+        var camForward = _mainCamera.transform.forward;
+        var camRight = _mainCamera.transform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+        var horizontalInput = camRight * hInput + camForward * vInput;
+        var horizontalVelocity = horizontalInput * PlayerSpeed;
+
+        if (horizontalInput != Vector3.zero) transform.forward = horizontalInput;
+
+        _playerVelocity = new Vector3(horizontalVelocity.x, _playerVelocity.y, horizontalVelocity.z);
+    }
+
+    private void Jump()
+    {
+        if (_isJumping)
+            _playerVelocity.y += Mathf.Log(JumpHeight * -PlayerSpeed * GravityValue);
+    }
+
+    private void StopFalling()
+    {
+        if (_playerVelocity.y < 0) _playerVelocity.y = 0;
+    }
+
+    private void Fall() => _playerVelocity.y += GravityValue * Time.deltaTime;
+
+    private bool ShouldMove() => _playerVelocity.magnitude > MovementThreshold;
+
+    private void MovePlayer() => _controller.Move(_playerVelocity * Time.deltaTime);
 }
