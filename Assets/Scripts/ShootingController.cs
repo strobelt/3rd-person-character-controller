6c44eb377b5a9278aa255060622ecfe2735effd0
Luiz Strobelt <lhsperez@gmail.com>
2020-10-20 00:48:38 -0300

    WIP

---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootingController : MonoBehaviour
+{
+    public GameObject Bullet;
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            var instance = Instantiate(Bullet, transform.position, transform.rotation);
+        }
+    }
+}
