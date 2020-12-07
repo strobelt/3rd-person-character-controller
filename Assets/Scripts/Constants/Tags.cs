9388d71bf4d24cfd338a9568254b487b303bc9af
Luiz Strobelt <lhsperez@gmail.com>
2020-12-06 23:25:39 -0300

    Add basic health bar to body part

---
+++ Assets/Scripts/Constants/Tags.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Scripts.Constants
+{
+    public class Tags
+    {
+        public const string HealthCanvas = "HealthCanvas";
+    }
+}
--- Assets/Scripts/Modifiers/DamageableModifier.cs
+++ Assets/Scripts/Modifiers/DamageableModifier.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Assets.Scripts.Constants;
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class DamageableModifier : MonoBehaviour, IHittable
 {
@@ -8,27 +11,84 @@ public class DamageableModifier : MonoBehaviour, IHittable
     public int DamageResistance = 3;
     public bool DecreaseOnHit = true;
     public bool Destroyable = true;
+    public float TimeToHideHealthBar = 2;
+
     public UnityEvent<GameObject, int> OnHit;
     public UnityEvent<GameObject> OnToughnessZero;
 
     private const int NoDamage = 0;
+    private Slider _healthBar;
+
+    private int _currentToughness;
+    private Canvas _healthCanvas;
+    private Coroutine _hidingHealthBar;
+
+    private int CurrentToughness
+    {
+        get => _currentToughness;
+        set
+        {
+            _currentToughness = value;
+            if (_healthBar != null)
+                _healthBar.value = _currentToughness;
+        }
+    }
+
+    void Start()
+    {
+        _healthCanvas = transform
+            .GetComponentsInChildren<Canvas>()
+            .FirstOrDefault(x => x.CompareTag(Tags.HealthCanvas));
+        if (_healthCanvas)
+        {
+            _healthCanvas.GetComponent<CanvasGroup>().alpha = 0;
+            _healthBar = _healthCanvas.GetComponentInChildren<Slider>();
+            _healthBar.minValue = 0;
+            _healthBar.maxValue = Toughness;
+        }
+
+        CurrentToughness = Toughness;
+    }
 
     public void Hit(GameObject hitter, int shootingDamage)
     {
         var partDamage = CalculateDamage(shootingDamage);
 
+        _healthCanvas.GetComponent<CanvasGroup>().alpha = 1;
+        if (_hidingHealthBar != null) StopCoroutine(_hidingHealthBar);
+        _hidingHealthBar = StartCoroutine(HideHealthBar());
+
         OnHit?.Invoke(gameObject, partDamage);
 
         if (DecreaseOnHit)
-            Toughness -= partDamage;
+            CurrentToughness -= partDamage;
 
-        if (Toughness <= 0)
+        if (CurrentToughness <= 0)
         {
             OnToughnessZero?.Invoke(gameObject);
             if (Destroyable) Destroy(gameObject);
         }
     }
 
+    private IEnumerator HideHealthBar()
+    {
+        yield return new WaitForSeconds(TimeToHideHealthBar);
+        _hidingHealthBar = StartCoroutine(FadeHealthBar());
+    }
+
+    private IEnumerator FadeHealthBar()
+    {
+        const float timeToFade = 2;
+        var elapsedTime = 0f;
+        var canvasGroup = _healthCanvas.GetComponent<CanvasGroup>();
+        while (elapsedTime < timeToFade)
+        {
+            elapsedTime += Time.deltaTime;
+
+            yield return canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0.0f, elapsedTime / timeToFade);
+        }
+    }
+
     private int CalculateDamage(int shootingDamage)
-        => Math.Max(shootingDamage - DamageResistance, NoDamage);
+            => Mathf.Max(shootingDamage - DamageResistance, NoDamage);
 }
